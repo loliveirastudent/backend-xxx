d93ec001d0704ac15d169515a5cd0d4dc6006a93
Lucas Oliveira <oliveirait.solutions@outlook.com>
2022-02-27 19:54:15 -0300

    first commit

---
+++ backend-xxx/src/main/java/br/com/oliveirait/backend/xxx/BackendXxxApplication.java
@@ -0,0 +1,13 @@
+package br.com.oliveirait.backend.xxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendXxxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendXxxApplication.class, args);
+	}
+
+}
---
+++ backend-xxx/src/test/java/br/com/oliveirait/backend/xxx/BackendXxxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oliveirait.backend.xxx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendXxxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
